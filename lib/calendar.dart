7a275ce1e9300c1eca5bdce5aab424b0a8096e6e
Zhengyi <ericzhaozzy@outlook.com>
2022-09-01 16:30:54 -0400

    calendar v1.3 (The dialog can update now; Date pickers now work properly

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -15,34 +15,126 @@ class _CalendarState extends State<Calendar> {
   final DateTime today = DateTime.now();
   DateTime startDate = DateTime.now();
   DateTime endDate = DateTime.now();
-  
-  void _selectDate(BuildContext context, bool isStart) async {
-    final DateTime? picked = await showDatePicker(
+
+  Future openDialog() => showCupertinoDialog(
       context: context,
-      initialDate: today,
-      firstDate: DateTime(today.year - 5, today.month, today.day),
-      lastDate: DateTime(today.year + 5, today.month, today.day),
-    );
-    if (picked != null) {
-      if (isStart) {
-        setState(() {
-          startDate = picked;
-        });
-      } else {
-        setState(() {
-          endDate = picked;
-        });
-      }
-    }
-  }
-  _showPickers() {
-    showDatePicker(
-      context: context,
-      initialDate: today,
-      firstDate: DateTime(today.year - 5, today.month, today.day),
-      lastDate: DateTime(today.year + 5, today.month, today.day),
-    );
-  }
+      builder: (context) => StatefulBuilder(
+          builder: (context, setState) => SimpleDialog(
+                title: const Text('Add an event'),
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30, right: 120, top: 10),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                          labelText: 'Event name',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 230, 104, 102),
+                              width: 1,
+                            ),
+                          ),
+                          isDense: true,
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 8)),
+                      onChanged: (val) {},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                          labelText: 'Event Description',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 230, 104, 102),
+                              width: 1,
+                            ),
+                          ),
+                          isDense: true,
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 8)),
+                      onChanged: (val) {},
+                    ),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Text('Start time'),
+                        Text('End time'),
+                      ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    ElevatedButton(
+                      child: Text(
+                          '${startDate.year}/${startDate.month}/${startDate.day}'),
+                      onPressed: () async {
+                        final DateTime? picked = await showDatePicker(
+                          context: context,
+                          initialDate: startDate,
+                          firstDate:
+                              DateTime(startDate.year - 5, startDate.month, startDate.day),
+                          lastDate:
+                              DateTime(startDate.year + 5, startDate.month, startDate.day),
+                        );
+                        if (picked != null) {
+                          setState(() {
+                            startDate = picked;
+                            if (startDate.compareTo(endDate) > 0) {
+                              endDate = startDate;
+                            }
+                          });
+                        }
+                      },
+                    ),
+                    const Text(' ~ '),
+                    ElevatedButton(
+                      child: Text(
+                          '${endDate.year}/${endDate.month}/${endDate.day}'),
+                      onPressed: () async {
+                        final DateTime? picked = await showDatePicker(
+                          context: context,
+                          initialDate: startDate,
+                          firstDate:
+                              DateTime(startDate.year, startDate.month, startDate.day),
+                          lastDate:
+                              DateTime(startDate.year + 5, startDate.month, startDate.day),
+                        );
+                        if (picked != null) {
+                          setState(() {
+                            endDate = picked;
+                          });
+                        }
+                      },
+                    ),
+                  ]),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        OutlinedButton(
+                            onPressed: () {
+                              startDate = today;
+                              endDate = today;
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel')),
+                        OutlinedButton(
+                            onPressed: () {
+                              
+                            },
+                            child: const Text('Reset')),
+                        ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Save')),
+                      ]),
+                ],
+              )));
 
   @override
   Widget build(BuildContext context) {
@@ -52,116 +144,7 @@ class _CalendarState extends State<Calendar> {
           actions: [
             IconButton(
               onPressed: () {
-                showCupertinoDialog(
-                    context: context,
-                    builder: (context) => SimpleDialog(
-                          title: const Text('Add an event'),
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 30, right: 120, top: 10),
-                              child: TextField(
-                                decoration: const InputDecoration(
-                                    labelText: 'Event name',
-                                    enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(10),
-                                      ),
-                                      borderSide: BorderSide(
-                                        color:
-                                            Color.fromARGB(255, 230, 104, 102),
-                                        width: 1,
-                                      ),
-                                    ),
-                                    isDense: true,
-                                    contentPadding: EdgeInsets.symmetric(
-                                        horizontal: 15, vertical: 8)),
-                                onChanged: (val) {},
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 30, vertical: 10),
-                              child: TextField(
-                                decoration: const InputDecoration(
-                                    labelText: 'Event Description',
-                                    enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(10),
-                                      ),
-                                      borderSide: BorderSide(
-                                        color:
-                                            Color.fromARGB(255, 230, 104, 102),
-                                        width: 1,
-                                      ),
-                                    ),
-                                    isDense: true,
-                                    contentPadding: EdgeInsets.symmetric(
-                                        horizontal: 15, vertical: 8)),
-                                onChanged: (val) {},
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: const [
-                                Text('Start time'),
-                                Text('End time'),
-                              ]
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                ElevatedButton(
-                                  child: Text('${startDate.year}/${startDate.month}/${startDate.day}'),
-                                  onPressed: () {
-                                    // _showPickers();
-                                    _selectDate(context, true);
-                                  },
-                                ),
-                                const Text(' ~ '),
-                                ElevatedButton(
-                                  child: Text('End Date'),
-                                  onPressed: () {},
-                                ),
-                              ]
-                            ),
-                            // SfDateRangePicker(
-                            //   // onSelectionChanged: ,
-                            //   selectionMode:
-                            //       DateRangePickerSelectionMode.multiple,
-                            //   initialSelectedDate: DateTime.now(),
-                            // ),
-
-                            // SizedBox(
-                            //   height: 150,
-                            //   width: 10,
-                            //   child: CupertinoDatePicker(
-                            //     initialDateTime: DateTime.now(),
-                            //     onDateTimeChanged:(dateTime) {
-                            //     // DateTime current = DateTime.now();
-                            //       setState(() {
-
-                            //       });
-                            //     },
-                            //   ),
-                            // ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                OutlinedButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('Cancel')),
-                                OutlinedButton(
-                                    onPressed: () {},
-                                    child: const Text('Reset')),
-                                ElevatedButton(
-                                    onPressed: () {},
-                                    child: const Text('Save')),
-                            ]),
-                          ],
-                        ));
+                openDialog();
               },
               // Design an more graceful add icon.
               icon: const Icon(Icons.add_task_rounded),
